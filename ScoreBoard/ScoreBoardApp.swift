62e9d6460837b2accbff3264ae5daa3a59f1a823
Kleyton <kleytonandroid@gmail.com>
2023-08-01 15:12:58 -0300

    Initial Commit

---
+++ ScoreBoard/ScoreBoardApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScoreBoardApp.swift
+//  ScoreBoard
+//
+//  Created by Kleyton Santos on 01/08/23.
+//
+
+import SwiftUI
+
+@main
+struct ScoreBoardApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
